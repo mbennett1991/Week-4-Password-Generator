e344582afcd74ff9c62147d2b7d042d48253ca10
Elle <bound_by_harmony@yahoo.com>
2020-12-17 13:18:31 +0000

    Initial Commit

---
+++ script.js
@@ -0,0 +1,36 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+function getRandomLower(){
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+
+function getRandomUpper(){
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+
+function getRandomNumber(){
+  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+
+function getRandomSymbol(){
+  var symbols = "!@#$%^&*(){}[]+<>/,.=-_~`.,";
+  return symbols[Math.floor(Math.random() * symbols.length)];
+}
+
+console.log(getRandomLower());
+console.log(getRandomUpper());
+console.log(getRandomNumber());
+console.log(getRandomSymbol());
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
