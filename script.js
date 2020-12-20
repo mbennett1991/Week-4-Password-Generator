b93c41bb76e83b83bfe503a0ab5073c79c4ef5ae
Elle <bound_by_harmony@yahoo.com>
2020-12-20 21:15:56 +0000

    Started Over - still not working

--- script.js
+++ script.js
@@ -1,66 +1,45 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-
-//Object to consolidate functions
-var randomFunctions = {
-  lower: getRandomLower,
-  upper: getRandomUpper,
-  number: getRandomNumber,
-  symbol: getRandomSymbol
-};
-
-//functions in object
-function getRandomLower() {
-  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
-}
-
-function getRandomUpper() {
-  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
-}
-
-function getRandomNumber() {
-  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
-}
-
-function getRandomSymbol() {
-  var symbols = "!@#$%^&*(){}[]+<>/,.=-_~`.,";
-  return symbols[Math.floor(Math.random() * symbols.length)];
-}
-
-//Console log to make sure they work
-console.log(getRandomLower());
-console.log(getRandomUpper());
-console.log(getRandomNumber());
-console.log(getRandomSymbol());
+var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var lowercase = "abcdefghijklmnopqrstuvwxyz";
+var numbers = "1234567890";
+var symbols = "!@#$%^&*(){}[]=/,.|~?";
 
 // Write password to the #password input
-function writePassword() {
-var confirmLower, confirmUpper, confirmSymbol, confirmNumbers;
+function writePassword(characterNums, confirmLower, confirmUpper, confirmSymbols, confirmNumbers) 
+{
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password").innerHTML=generatePassword;
+
+  passwordText.value = password;
+  
+  var confirmLower, confirmUpper, confirmSymbols, confirmNumbers;
 
   var characterNums = prompt("How many characters would you like to add?");
   if (characterNums >= 8 && characterNums < 128) {
     confirmLower = confirm("Would you like to add lowercase letters to password?");
+      if (confirmLower) characterNums = characterNums.concat(lowercase)
     confirmUpper = confirm("Would you like to add uppercase letters to password?");
-    confirmSymbol = confirm("Would you like to include symbols in password?");
+      if (confirmUpper) characterNums = characterNums.concat(uppercase)
+    confirmSymbols = confirm("Would you like to include symbols in password?");
+      if (confirmSymbols) characterNums = characterNums.concat(symbols)
     confirmNumbers = confirm("Would you like to add numbers to password?")
+      if (confirmNumbers) characterNums = characterNums.concat(numbers)
   }
 
   else {
     prompt("Please select a number between 8 and 128.");
-  };
 
 
-  var password = generatePassword(characterNums, confirmLower, confirmUpper, confirmSymbol, confirmNumbers);
-  var passwordText = document.querySelector("#password");
+  var generatePassword = " ";
 
-  passwordText.value = password;
+  for (i = 1; i <= characterNums; i++)
+    var pword = Math.floor(Math.random() * characterNums.length + 1)
 
-};
-
-function generatePassword(characterNums, confirmLower, confirmUpper, confirmSymbol, confirmNumbers) {
-  for (i = 0; i < characterNums; i++);
-};
-
+    generatePassword += characterNums.charAt(characterNums)
+  }
+  return generatePassword
+}
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
